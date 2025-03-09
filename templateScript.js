2bf4ce1b706efa3e5d9cde8dad8e0eadf4b8ae2f
Ole <ole.hayn58@gmail.com>
2025-03-09 17:26:58 +0100

    added ability to set templates

--- templateScript.js
+++ templateScript.js
@@ -1,8 +1,63 @@
 const saveButton = document.getElementById('save-button')
 const resetButton = document.getElementById('reset-button')
 
-const exampleUrl = 'example.com'
-const exampleTitle = 'Example written by Ole370'
+const templateInput = document.getElementById('template-input')
+const previewBox = document.getElementById('preview-box')
+
+const exampleBookmark = {
+    url: 'https://example.com',
+    title: 'Example written by Ole370'
+}
 const exampleIndex = 1
 
-let template =
+const page = document.getElementById('page')
+
+function addAlertBox(message, strongMessage = "Info! ") {
+  alertBox = document.createElement("div")
+  alertBox.setAttribute("class", "alert")
+  alertBox.innerHTML = `<span class="close-button" >&times;</span><strong>${strongMessage}</strong> ${message}`
+  page.append(alertBox)
+
+  closeButtons = document.getElementsByClassName('close-button')
+  for (let i = 0; i < closeButtons.length; i++) {
+      closeButtons[i].onclick = function(){
+      let div = this.parentElement;
+      div.style.opacity = "0";
+      setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
+}
+}
+
+let template;
+browser.storage.local.get(['template'])
+.then((result) => {
+  template = result.template
+  setPreviewFromTemplate(template)
+  templateInput.value = template
+})
+.catch((error) => {
+  console.error(error);
+});
+
+
+function setPreviewFromTemplate(template) {
+    templatedString = fillTemplateWithBookmark(template, exampleBookmark, exampleIndex)
+
+    previewBox.innerHTML = templatedString
+}
+
+templateInput.addEventListener("change", () => {
+    templateString = templateInput.value
+    setPreviewFromTemplate(templateString)
+});
+
+resetButton.addEventListener("click", () => {
+    templateInput.value = resetTemplate
+    setTemplate(resetTemplate)
+    addAlertBox("Successfully reset template.")
+})
+
+saveButton.addEventListener("click", () => {
+    setTemplate(templateInput.value)
+    addAlertBox(`Set new template: "${templateInput.value}"`)
+})
--- templateStorage.js
+++ templateStorage.js
@@ -18,20 +18,9 @@ browser.storage.local.get(['template'])
 function setTemplate(templateString) {
     browser.storage.local.set({ template: templateString })
         .then(() => {
-            console.log('Set new template: ', templateString,)
+            console.log('Set new template: "', templateString, '"')
         })
         .catch((error) => {
             console.error(error);
         });
-}
-
-function getTemplate() {
-  browser.storage.local.get(['template'])
-  .then((result) => {
-    return result.template
-  })
-  .catch((error) => {
-    console.error(error);
-    return -1
-  });
 }
